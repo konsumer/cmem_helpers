5bc2dbfe5476846b0267624851de285e29e50203
David Konsumer <konsumer@jetboystudio.com>
2023-08-11 00:53:09 -0700

    fix indentation in wasm

--- src/wasm/test.c
+++ src/wasm/test.c
@@ -2,18 +2,18 @@
 #include <string.h>
 
 typedef struct Color {
-	unsigned char r;
-	unsigned char g;
-	unsigned char b;
-	unsigned char a;
+  unsigned char r;
+  unsigned char g;
+  unsigned char b;
+  unsigned char a;
 } Color;
 
 // exports
 void test1(char* name, char* destination) {
-	strcpy(destination, "Hello ");
-    strcat(destination, name);
+  strcpy(destination, "Hello ");
+  strcat(destination, name);
 }
 
 void test2(Color* color) {
-	color->a = 100;
+  color->a = 100;
 }
