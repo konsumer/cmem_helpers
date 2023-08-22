7c26837a611706d7ee79df5c56e7df8e35cc9464
David Konsumer <konsumer@jetboystudio.com>
2023-08-21 23:02:03 -0700

    more testing

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,6 @@
 import memhelpers from './index.js'
 import { readFile } from 'fs/promises'
+import aEq from 'arraybuffer-equal'
 
 const wasmBytes = await readFile('src/test.wasm')
 
@@ -34,6 +35,12 @@ const Color = struct({
   a: 'Uint8'
 })
 
+const Vector3 = struct({
+  x: 'Int32',
+  y: 'Int32',
+  z: 'Int32'
+})
+
 test('should be able to receive/return a string from WASM function', () => {
   const retPtr = mod.malloc(20)
   mod.test1(setString('World'), retPtr)
@@ -41,9 +48,22 @@ test('should be able to receive/return a string from WASM function', () => {
   expect(response).toBe('Hello World')
 })
 
-test('should be able to work with a struct', () => {
+test('should be able to work with an u8 struct', () => {
   const color = Color()
   color.a = 255
   mod.test2(color._address)
+  const buf = memory.buffer.slice(color._address, color._address + color._size)
+  const ref = new Uint8Array([0x10, 0x02, 0x02, 0x64]).buffer
+  expect(aEq(buf, ref)).toBeTruthy()
   expect(color.a).toBe(100)
+})
+
+test('should be able to work with an i32 struct', () => {
+  const v = Vector3()
+  v.x = 0
+  v.y = 10
+  v.z = -100
+  const buf = memory.buffer.slice(v._address, v._address + v._size)
+  const ref = new Uint8Array([0x00, 0x00, 0x00, 0x00, 0x0a, 0x00, 0x00, 0x00, 0x9c, 0xff, 0xff, 0xff]).buffer
+  expect(aEq(ref, buf)).toBeTruthy()
 })
